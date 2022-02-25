380c582a5c0d9a0a45656e6a428d7100350ac643
TheDoctor0 <dawid.janik95@gmail.com>
2022-02-25 16:02:21 +0100

    Support topics list.

--- src/SESConfirmWebhookMiddleware.php
+++ src/SESConfirmWebhookMiddleware.php
@@ -4,9 +4,8 @@
 namespace Megaverse\LaravelSesManager;
 
 use Illuminate\Http\Request;
-use Megaverse\LaravelSesManager\Contracts\SESMessageValidatorContract;
-use Megaverse\LaravelSesManager\Exceptions\WrongWebhookRouting;
 use Symfony\Component\HttpFoundation\Response;
+use Megaverse\LaravelSesManager\Contracts\SESMessageValidatorContract;
 
 class SESConfirmWebhookMiddleware
 {
@@ -28,9 +27,8 @@ class SESConfirmWebhookMiddleware
    */
   function handle (Request $request, $next) {
       if (
-          !$request->hasHeader('X-Amz-Sns-Topic-Arn')  &&
-          (config('ses-manager.sns.bounce_topic_arn') !== $request->header('X-Amz-Sns-Topic-Arn') ||
-              config('ses-manager.sns.complaint_topic_arn') !== $request->header('X-Amz-Sns-Topic-Arn'))
+          !$request->hasHeader('X-Amz-Sns-Topic-Arn') ||
+          !in_array($request->header('X-Amz-Sns-Topic-Arn'), config('ses-manager.sns.topic_arn', []))
       ) {
           return new Response('Bad Request', 400);
       }
